1dcff61f27ed3006c4a3f10704889772c5355556
Suroj Maharjan <surojcodes@gmail.com>
2023-11-28 20:57:51 -0600

    Get todos done

--- todos.controller.js
+++ todos.controller.js
@@ -1,8 +1,24 @@
+const tableName = 'todos';
 const createTodo = (req, res) => {
   res.send('Create a Todo');
 };
-const getTodos = (req, res) => {
-  res.send('Get all todos');
+const getTodos = async (req, res) => {
+  try {
+    const result = await req.app.locals.db.query(
+      `SELECT TOP 5 * FROM ${tableName}`
+    );
+    const todos = result.recordset;
+    res.status(200).json({
+      success: true,
+      data: todos,
+    });
+  } catch (err) {
+    console.log('Error while fetching todos:', err);
+    res.status(500).json({
+      success: false,
+      data: 'Error fetching todos',
+    });
+  }
 };
 const getTodo = (req, res) => {
   res.send('Get a todo');
